163c1b5714ebce7294694565aa215315b40d9b7c
Rosita Gerbo <48881429+rho9@users.noreply.github.com>
2020-11-29 20:15:01 +0100

    add management of the first 5 words in the grids (first row)

--- create_grid.py
+++ create_grid.py
@@ -5,16 +5,22 @@ from PIL import ImageDraw
 img = Image.open('images/white_card.png', 'r')
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype("arial.ttf", 50)
-text = "carta1"
-W, H = 120, 142  # first card position
-draw.text((W,H), text, fill="black", font=font)
-
-img_w, img_h = img.size
 # white background
-background = Image.new('RGBA', (1440, 900), (255, 255, 255, 255))
+background = Image.new('RGBA', (2000, 1500), (255, 255, 255, 255))
+
+font = ImageFont.truetype("arial.ttf", 50)
+word_list = ["carta1", "carta2", "carta3", "carta4", "carta5"]
+W, H = 120, 142  # first card position
+img_w, img_h = img.size
 bg_w, bg_h = background.size
-background.paste(img)
-offset = (img_w + 10, 0)
-background.paste(img, offset)
+
+for word in word_list:
+    img = Image.open('images/white_card.png', 'r')
+    draw = ImageDraw.Draw(img)
+    draw.text((W,H), word, fill="black", font=font)
+    pos = word_list.index(word)
+    # offset pdm style
+    offset = (pos * img_w + (pos * img_w + 10)//9 +30, 30)
+    background.paste(img, offset)
+
 background.save('images/grid.png')
