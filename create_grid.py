8591a97283ec9d5cca2feb721c130b31ac010a2c
Rosita Gerbo <48881429+rho9@users.noreply.github.com>
2020-11-30 22:00:54 +0100

    add todo comments and update white space between cards

--- create_grid.py
+++ create_grid.py
@@ -6,7 +6,7 @@ img = Image.open('images/white_card.png', 'r')
 draw = ImageDraw.Draw(img)
 
 # white background
-background = Image.new('RGBA', (2000, 1500), (255, 255, 255, 255))
+background = Image.new('RGBA', (2000, 1700), (255, 255, 255, 255))  # background dimensione in base alla dimensione e al numero di carte e lo parametrizzi
 
 font = ImageFont.truetype("arial.ttf", 50)
 word_list = ["carta1", "carta2", "carta3", "carta4", "carta5"]
@@ -15,12 +15,11 @@ img_w, img_h = img.size
 bg_w, bg_h = background.size
 
 for word in word_list:
-    img = Image.open('images/white_card.png', 'r')
+    img = Image.open('images/white_card.png', 'r')  # controlla se è da chiudere
     draw = ImageDraw.Draw(img)
     draw.text((W,H), word, fill="black", font=font)
     pos = word_list.index(word)
-    # offset pdm style
-    offset = (pos * img_w + (pos * img_w + 10)//9 +30, 30)
+    offset = (pos * img_w +(pos+1)*30, 30)  # controlla cosa sono: pixel?
     background.paste(img, offset)
 
 background.save('images/grid.png')
