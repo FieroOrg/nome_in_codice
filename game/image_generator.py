8abbc93b2602cee3de5e6d5f7d972453c8cf0253
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2020-12-05 21:04:16 +0100

    create image_generator class

---
+++ game/image_generator.py
@@ -0,0 +1,32 @@
+class ImageGenerator():
+    """Class description
+
+    """
+
+    def __init__(self):
+        self.col_num = 5
+
+    def image_spy(self, words):
+        """
+
+        :param words: list of Word
+        :return: immagine (path??)
+        """
+        # not colored + hide found one
+        print("hello")
+        # return '/path/to/return'
+
+    def image_master(self, words):
+        """
+
+        :param words: list of Word
+        :return: immagine (path??)
+        """
+        # colored + hide found one
+        return '/path/to/return'
+
+
+img_gen = ImageGenerator()
+img_gen.image_spy([])
+# if you need to do comparisons use colorgame.py (create method is_blue etc?)
+# before creating a new method check it doesn't exist in word.py
