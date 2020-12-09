1f6be22f0f9bf6ef80ac13f16d4229cda4836ec7
Rosita Gerbo <48881429+rho9@users.noreply.github.com>
2020-12-10 00:41:15 +0100

    manage the addition of a card to the background with a method

--- game/image_generator.py
+++ game/image_generator.py
@@ -14,6 +14,7 @@ class ImageGenerator():
     def __init__(self):
         self.col_num = 5
         self.space_btw_cards = 30
+        self.font = ImageFont.truetype("arial.ttf", 50)
 
     def image_spy(self, words):
         """
@@ -22,42 +23,18 @@ class ImageGenerator():
         :return: image path
         """
         background = self.create_bg('../res/images/white_card.png', words)
-        font = ImageFont.truetype("arial.ttf", 50)
         for word in words:
-            img = Image.open('../res/images/white_card.png', 'r')
-            img_w, img_h = img.size
-            draw = ImageDraw.Draw(img)
-
-            # calculate where to place the word inside the white space of the card
-            (word_width, baseline), (offset_x, offset_y) = font.font.getsize(word.name)
-            # pixel number in which the white space in the card starts and ends
-            start_pixel = 40
-            end_pixel = 335
-            available_space = end_pixel - start_pixel
-            free_space = available_space - word_width
-            # W, H: top left position of the word in the white space of the card
-            W, H = (40 + (free_space // 2)), 142
-            if word.revealed and word.color==colorgame.ColorGame.RED:
-                color = "red"
-            elif word.revealed and word.color==colorgame.ColorGame.BLUE:
-                color = "blue"
-            elif word.revealed and word.color==colorgame.ColorGame.WHITE:
-                color = "gold"
-            else:
-                color = "black"
-            draw.text((W, H), word.name, fill=color, font=font)
-
             pos = words.index(word)
-            offset = (pos%5 * img_w + (pos%5 + 1) * 30, (pos//5) * img_h + ((pos//5) + 1) * 30)  # pixel coordinates
-            background.paste(img, offset)
+            self.add_card_to_bg(background, '../res/images/white_card.png', word, pos)
+
         background.save('../res/images/grid.png')
         # return '/path/to/return'
 
     """
     This function creates the background of the image
     """
-    def create_bg(self, image_path, words):
-        with Image.open(image_path) as img:
+    def create_bg(self, img_path, words):
+        with Image.open(img_path) as img:
             img_w, img_h = img.size
 
             bg_width = self.col_num * (img_w + self.space_btw_cards) + self.space_btw_cards
@@ -69,6 +46,50 @@ class ImageGenerator():
 
             # Image constructor: mode, size (width, height in pixels), color.
             background = Image.new('RGBA', (bg_width, bg_height), (255, 255, 255, 255))
+        return background
+
+    """
+    This function add a card to the background
+    """
+    def add_card_to_bg(self, background, img_path, word, pos):
+        """
+
+        :param background: the background on which to add the card
+        :param img_path: the path of the card to add
+        :param word: the word to add
+        :param pos: the position of the word in the list
+        :return: the background with the new card
+        """
+        with Image.open(img_path) as img:
+            img_w, img_h = img.size
+            draw = ImageDraw.Draw(img)
+        # calculate where to place the word inside the white space of the card
+        (word_width, baseline), (offset_x, offset_y) = self.font.font.getsize(word.name)
+        # pixel number in which the white space in the card starts and ends
+        start_pixel = 40
+        end_pixel = 335
+        available_space = end_pixel - start_pixel
+        free_space = available_space - word_width
+        # x, y: top-left position of the word in the card white space according to the word's length
+        x, y = (40 + (free_space // 2)), 142
+
+        # choose the color of the card
+        if word.revealed and word.color == colorgame.ColorGame.RED:
+            color = "red"
+        elif word.revealed and word.color == colorgame.ColorGame.BLUE:
+            color = "blue"
+        elif word.revealed and word.color == colorgame.ColorGame.WHITE:
+            color = "gold"
+        else:
+            color = "black"
+
+        draw.text((x, y), word.name, fill=color, font=self.font)
+
+        # pixel coordinates in the background of the card top-left corner
+        card_x = (pos % self.col_num) * img_w + (pos % self.col_num + 1) * self.space_btw_cards
+        card_y = (pos // self.col_num) * img_h + ((pos // self.col_num) + 1) * self.space_btw_cards
+        offset = (card_x, card_y)
+        background.paste(img, offset)
         return background
 
     def image_master(self, words):
