116c1b4f23f559517f5e25cc05a4e9440da91492
Rosita Gerbo <48881429+rho9@users.noreply.github.com>
2020-12-14 11:33:22 +0100

    refactor the writing and the adding of the cards

--- game/image_generator.py
+++ game/image_generator.py
@@ -28,22 +28,25 @@ class ImageGenerator:
         for word in words:
             pos = words.index(word)
             if word.revealed:
-                background = self.add_revealed_card(background, word, pos)
+                # substitute the path with the correct word property
+                background = self.add_card(background, '../res/images/cards/black_card.png', pos)
             elif self.master:
                 # choose the color of the card
                 if word.color == colorgame.ColorGame.RED:
-                    path_card = '../res/images/cards/red_card.png'
+                    card_path = '../res/images/cards/red_card.png'
                 elif word.color == colorgame.ColorGame.BLUE:
-                    path_card = '../res/images/cards/blue_card.png'
+                    card_path = '../res/images/cards/blue_card.png'
                 elif word.color == colorgame.ColorGame.WHITE:
-                    path_card = '../res/images/cards/white_card.png'
+                    card_path = '../res/images/cards/white_card.png'
                 else:
-                    path_card = '../res/images/cards/black_card.png'
-                background = self.add_card(background, path_card, word, pos)
+                    card_path = '../res/images/cards/black_card.png'
+                written_card_path = self.write_on_card(card_path, word)
+                background = self.add_card(background, written_card_path, pos)
             else:
-                pos = words.index(word)
-                background = self.add_card(background, '../res/images/cards/white_card.png', word, pos)
+                written_card_path = self.write_on_card('../res/images/cards/white_card.png', word)
+                background = self.add_card(background, written_card_path, pos)
         background.save('../res/images/grid.png')
+        # empty temp dir
         return '../res/images/grid.png'
 
     """
@@ -69,57 +72,49 @@ class ImageGenerator:
     """
     This function add a card to the background
     """
-    def add_card(self, background, img_path, word, pos):
+    def write_on_card(self, card_path, word):
         """
 
         :param background: the background on which to add the card
-        :param img_path: the path of the card to add
+        :param card_path: the path of the card to add
         :param word: the word to add
         :param pos: the position of the word in the list
         :return: the background with the new card
         """
-        with Image.open(img_path) as img:
-            img_w, img_h = img.size
+        with Image.open(card_path) as img:
             draw = ImageDraw.Draw(img)
-        # calculate where to place the word inside the white space of the card
-        (word_width, word_height), (offset_x, offset_y) = self.font.font.getsize(word.name)
-        # coordinates of the white space
-        x_left = 18
-        x_right = 160
-        y_up = 65
-        y_down = 98
-        # dimension of the white space
-        white_space_width = x_right - x_left
-        white_space_height = y_down - y_up
-        # white space that remains free
-        free_space_x = white_space_width - word_width
-        free_space_y = white_space_height - 23
-        # 23 and not word_height because sometimes it is 18 and sometimes it is 23
-        # and then the words are on different planes and they suck
-        # (maybe if there is a letter going down the height increases).
-        # coordinates of the beginning of the word
-        word_x_pos = x_left + (free_space_x // 2)
-        word_y_pos = y_up + (free_space_y // 2)
+            # calculate where to place the word inside the white space of the card
+            (word_width, word_height), (offset_x, offset_y) = self.font.font.getsize(word.name)
+            # coordinates of the white space
+            x_left = 18
+            x_right = 160
+            y_up = 65
+            y_down = 98
+            # dimension of the white space
+            white_space_width = x_right - x_left
+            white_space_height = y_down - y_up
+            # white space that remains free
+            free_space_x = white_space_width - word_width
+            free_space_y = white_space_height - 23
+            # 23 and not word_height because sometimes it is 18 and sometimes it is 23
+            # and then the words are on different planes and they suck
+            # (maybe if there is a letter going down the height increases).
+            # coordinates of the beginning of the word
+            word_x_pos = x_left + (free_space_x // 2)
+            word_y_pos = y_up + (free_space_y // 2)
 
-        # choose the color of the text
-        if self.master and word.color == colorgame.ColorGame.ASSASSIN:
-            font_color = "white"
-        else:
-            font_color = "black"
-        draw.text((word_x_pos, word_y_pos), word.name, fill=font_color, font=self.font)
+            # choose the color of the text
+            if self.master and word.color == colorgame.ColorGame.ASSASSIN:
+                font_color = "white"
+            else:
+                font_color = "black"
+            draw.text((word_x_pos, word_y_pos), word.name, fill=font_color, font=self.font)
+            img.save('../res/images/cards/temp/written_card.png')
+        return '../res/images/cards/temp/written_card.png'
 
-        # top-left corner coordinates of the card in the background
-        card_x = (pos % self.col_num) * img_w + (pos % self.col_num + 1) * self.space_btw_cards
-        card_y = (pos // self.col_num) * img_h + ((pos // self.col_num) + 1) * self.space_btw_cards
-        offset = (card_x, card_y)
-        background.paste(img, offset)
-        return background
-
-    def add_revealed_card(self, background, word, pos):
-        with Image.open('../res/images/cards/black_card.png') as img:
-            # substitute path with correct word property
+    def add_card(self, background, card_path, pos):
+        with Image.open(card_path) as img:
             img_w, img_h = img.size
-            draw = ImageDraw.Draw(img)
             # top-left corner coordinates of the card in the background
             card_x = (pos % self.col_num) * img_w + (pos % self.col_num + 1) * self.space_btw_cards
             card_y = (pos // self.col_num) * img_h + ((pos // self.col_num) + 1) * self.space_btw_cards
